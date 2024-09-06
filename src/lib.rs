e7b4aaf5e0b8b8f6d01c149ad51d556157f86e0c
rafiyq <rafiyq@outlook.com>
2024-09-06 18:22:52 +0000

    Create countdown timer

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 mod templates;
 
 use axum::{routing::get, Router};
+use chrono::{DateTime, Utc};
 use tower_service::Service;
 use worker::{event, Context, Env, HttpRequest, Result};
+
+const DT_UNDANGAN: &str = "2024-10-27T08:00:00+07:00";
 
 fn router() -> Router {
     Router::new()
         .route("/", get(index))
-        .nest_service("/countdown", get(index))
+        // .route_layer("/update", get(update))
 }
 
 #[event(fetch)]
@@ -21,12 +24,26 @@ async fn fetch(
 }
 
 async fn index() -> templates::Index {
-    
-    // sleep(Duration::from_secs(1)).await;
+    let dt_remaining = DateTime::parse_from_rfc3339(DT_UNDANGAN).unwrap()
+        .with_timezone(&Utc)
+        .signed_duration_since(Utc::now());
+
+    let day_time = templates::DayAndTime::from_timedelta(dt_remaining);
 
     templates::Index {
         title: "Beranda".to_string(), 
         guestname: "Dian".to_string(),
-        datetime_remaining: 102
+        countdown_ongoing: !day_time.is_timeout(),
+        countdown_remaining: day_time,
+    }
+}
+
+fn update() -> templates::Countdown {
+    let dt_remaining = DateTime::parse_from_rfc3339(DT_UNDANGAN).unwrap()
+        .with_timezone(&Utc)
+        .signed_duration_since(Utc::now());
+    
+    templates::Countdown { 
+        countdown_remaining: templates::DayAndTime::from_timedelta(dt_remaining) 
     }
 }
--- src/templates.rs
+++ src/templates.rs
@@ -1,9 +1,50 @@
+use std::fmt::{self, Formatter, Display};
 use askama_axum::Template;
+use chrono::TimeDelta;
+
+pub struct DayAndTime {
+    days: u32,
+    hours: u32,
+    minutes: u32,
+    seconds: u32
+}
+
+impl DayAndTime {
+    pub fn from_timedelta(timedelta: TimeDelta) -> Self {
+        DayAndTime {
+            days: timedelta.num_days() as u32,
+            hours: (timedelta.num_hours() % 24) as u32,
+            minutes: (timedelta.num_minutes() % 60) as u32,
+            seconds: (timedelta.num_seconds() % 60) as u32
+        }
+
+    }
+    pub fn is_timeout(&self) -> bool {
+        self.days == 0 && 
+        self.hours == 0 && 
+        self.minutes == 0 && 
+        self.seconds == 0
+    }
+}
+
+impl Display for DayAndTime {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}-{}:{}:{}",
+            self.days, self.hours, self.minutes, self.seconds)
+    }
+}
 
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct Index {
     pub title: String,
     pub guestname: String,
-    pub datetime_remaining: u8,
+    pub countdown_ongoing: bool,
+    pub countdown_remaining: DayAndTime
+}
+
+#[derive(Template)]
+#[template(path = "countdown.html")]
+pub struct Countdown {
+    pub countdown_remaining: DayAndTime
 }
