386df0da2c5df6079d997570d6a3e7e231a74cb7
rafiyq <rafiyq@outlook.com>
2024-09-12 12:08:22 +0700

    Use fenix overlay

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,35 +1,49 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+let 
+  # Rust toolchains for Nix. see: https://github.com/nix-community/fenix
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
+in {
 
   # Which nixpkgs channel to use.
-  channel = "unstable"; # or "stable-23.11"
+  channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-  packages = with pkgs; [
-    cargo
-    rustc
-    rustfmt
-    stdenv.cc
-    wasm-pack
-    wasm-opt
-    worker-build
-    wrangler
+  packages = [
+    (with fenix; with stable; combine [
+      cargo
+      clippy
+      rust-src
+      rustc
+      rustfmt
+      targets.wasm32-unknown-unknown.stable.rust-std
+    ])
+    pkgs.nodejs
+    pkgs.stdenv.cc
   ];
 
   # Sets environment variables in the workspace
   env = {
-    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+    RUST_SRC_PATH = "${fenix.stable.rust-src}/lib/rustlib/src/rust/library";
   };
 
   # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
   idx.extensions = [
     "rust-lang.rust-analyzer"
     "tamasfe.even-better-toml"
-    "serayuzgur.crates"
+    "fill-labs.dependi"
     "vadimcn.vscode-lldb"
   ];
+
+  # Commands to execute when the workspace is created and opened for the first time.
+  idx.workspace.onCreate = {
+    npm-update = "npm install -g npm@latest";
+    wrangler-install = "npm install -g wrangler@latest";
+    worker-build-install = "cargo install worker-build";
+    path-update = "PATH=$PATH:$HOME/.cargo/bin";
+  };
 
   # Enable previews and customize configuration
   idx.previews = {
