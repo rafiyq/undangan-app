e67ce984b9ab418878a049a8805fb6cf0b3713c7
rafiyqw <rafiyqw@gmail.com>
2024-09-17 04:31:36 +0000

    Make duration from str

--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,8 @@ pub fn make_duration(dt: &str) -> TimeDelta {
         .with_timezone(&Utc)
         .signed_duration_since(Utc::now())
 }
+
+pub fn make_duration_from_str(dt: &str) -> TimeDelta {
+    let end_dt = DateTime::parse_from_str(dt, "%v %T %z").unwrap().to_utc();
+    end_dt - Utc::now()
+}
