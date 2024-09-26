2a6d4d09e7cbac97a73ff661169c685bd8acfd8f
rafiyqw <rafiyqw@gmail.com>
2024-09-26 03:23:18 +0000

    Use tailwind style to manage css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,20 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '6rem',
-        '2xl': '8rem',
+    extend: {
+      boxShadow: {
+        'nm-flat': '0.2em 0.2em calc(0.2em * 2) #d6d3c8, calc(0.2em * -1) calc(0.2em * -1) calc(0.2em * 2) #ffffff',
+        'nm-pressed': 'inset 0.2em 0.2em calc(0.2em * 2) #d6d3c8, inset calc(0.2em * -1) calc(0.2em * -1) calc(0.2em * 2) #ffffff'
+      },
+      colors: {
+        'colour1': '#F9F5E9',
+        'colour2': '#BF814B',
+        'colour3': '#735F46',
+        'colour4': '#9AA68F',
+        'colour5': '#BFA65A',
+        'colour6': '#BF8654',
       },
     },
-    extend: {},
   },
   plugins: [],
 }
