b873a6af112fc7b74ba9d59b121927284d993d58
rafiyqw <rafiyqw@gmail.com>
2024-10-03 03:07:14 +0000

    Include rust file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./templates/**/*.html"];
+export const content = ["./src/**/*.rs"];
 export const theme = {
   extend: {
     boxShadow: {
