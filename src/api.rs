0617587ad0d015df532efe88df06235f7c883a81
rafiyqw <rafiyqw@gmail.com>
2024-10-16 03:36:49 +0000

    No error in api

--- src/api.rs
+++ src/api.rs
@@ -29,7 +29,7 @@ pub async fn get_guest(username: String) -> Result<Option<Guest>, ServerFnError>
 
 #[cfg_attr(feature = "ssr", worker::send)]
 #[server(GetBucket)]
-pub async fn get_bucket(filename: String) -> Result<worker::Response, ServerFnError> {
+pub async fn get_bucket(filename: String) -> Result<Vec<u8>, ServerFnError> {
     use axum::Extension;
     use leptos_axum::extract;
     use std::sync::Arc;
@@ -37,18 +37,15 @@ pub async fn get_bucket(filename: String) -> Result<worker::Response, ServerFnEr
     
     let Extension(env): Extension<Arc<Env>> = extract().await?;
     let bucket = env.bucket("bucket")?;
-
     let object = bucket.get(filename).execute().await?;
-    let body = object.body().unwrap().bytes().await?;
-    let http_header = object.http_metadata().content_type.unwrap_or("application/octet-stream".to_string());
-    let mut headers = Headers::new();
-    headers.set("Content-Type", &http_header)?;
-    Ok(Response::from_bytes(body)?.with_headers(headers))
+    let body = object.unwrap().body().unwrap().bytes().await?; 
+
+    Ok(body)
 }
 
 #[cfg_attr(feature = "ssr", worker::send)]
 #[server(GetKV)]
-pub async fn get_kv(key: String) -> Result<worker::Response, ServerFnError> {
+pub async fn get_kv(key: String) -> Result<Option<Vec<u8>>, ServerFnError> {
     use axum::Extension;
     use leptos_axum::extract;
     use std::sync::Arc;
@@ -56,31 +53,16 @@ pub async fn get_kv(key: String) -> Result<worker::Response, ServerFnError> {
     
     let Extension(env): Extension<Arc<Env>> = extract().await?;
     let kv = env.kv("static")?;
-    let value = kv.get(key).bytes().await?;
+    let value = kv.get(&key).bytes().await?;
 
-    let mime = asset_str.rsplit_once('.').map_or_else(
-        || "text/plain",
-        |(_, ext)| match ext {
-            "css" => "text/css",
-            "html" => "text/html",
-            "js" => "application/javascript",
-            "json" => "application/json",
-            "ico" => "image/x-icon",
-            "xml" => "application/xml",
-            _ => "text/plain",
-        },
-    );
-
-    let mut headers = Headers::new();
-    headers.set("Content-Type", mime)?;
-    Ok(Response::from_bytes(file_content)?.with_headers(headers))
+    Ok(value)
 }
 
 #[cfg(feature = "ssr")]
 pub fn register_server_functions() {
     use leptos::server_fn::axum::register_explicit;
 
-    register_explicit::<GetGuest>();
     register_explicit::<GetBucket>();
+    register_explicit::<GetGuest>();
     register_explicit::<GetKV>();
 }
