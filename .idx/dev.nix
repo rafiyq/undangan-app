a882cac8a7f8a77a3607de4039a70da4fb063673
rafiyqw <rafiyqw@gmail.com>
2025-01-13 01:22:10 +0000

    Use native rustup

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,29 +1,21 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-
-{ pkgs, ... }: 
-let 
-  # Rust toolchains for Nix
-  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
-in {
+{ pkgs, ... }: {
 
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    (with fenix; combine [
-      minimal.cargo
-      minimal.rustc
-      targets.wasm32-unknown-unknown.latest.rust-std
-    ])
-    pkgs.nodejs
     pkgs.stdenv.cc
+    pkgs.nodejs
+    pkgs.rustup
+    pkgs.leptosfmt
   ];
 
   # Sets environment variables in the workspace
   env = {
-    RUST_SRC_PATH = "${fenix.latest.rust-src}/lib/rustlib/src/rust/library";
+    RUSTUP_HOME = "$HOME/.rustup";
     PATH = ["$HOME/.cargo/bin"];
   };
 
@@ -38,9 +30,11 @@ in {
 
   # Commands to execute when the workspace is created and opened for the first time.
   idx.workspace.onCreate = {
+    rust-install = "rustup default nightly; rustup target add wasm32-unknown-unknown";
     npm-update = "npm install -g npm@latest";
     wrangler-install = "npm install -g wrangler@latest";
-    tailwindcss-install = "npm install -g tailwindcss@latest";
+    # tailwindcss-install = "npm install -g tailwindcss@latest";
+    # tailwindcss-v4beta-install = "npm install -g tailwindcss@next @tailwindcss/cli@next";
     cargo-leptos-install = "cargo install cargo-leptos --locked";
     worker-build-install = "cargo install worker-build";
   };
